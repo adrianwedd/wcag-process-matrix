e2afe2aa4522da38988ea2bb66ecb30fc27486f0
Stephane Deschamps <stephane.deschamps@gmail.com>
2018-03-21 09:36:13 +0100

    Reorganizing folders

--- tests/complete_table.php
+++ tests/complete_table.php
@@ -20,17 +20,17 @@
 
 // we load contents
 // all the WCAG information
-$url = "data/wcag.json";
+$url = "../data/wcag2.0/wcag.json";
 $contents = file_get_contents($url);
 $contents = utf8_encode($contents);
 $wcag = json_decode($contents, true);
 // responsibility matrix
-$url = "data/responsibility_matrix.json";
+$url = "../data/wcag2.0/responsibility_matrix.json";
 $contents = file_get_contents($url);
 $contents = utf8_encode($contents);
 $matrix = json_decode($contents, true);
 // labels for responsibility matrix
-$url = "data/responsibility_labels.json";
+$url = "../data/responsibility_labels.json";
 $contents = file_get_contents($url);
 $contents = utf8_encode($contents);
 $labels = json_decode($contents, true);
