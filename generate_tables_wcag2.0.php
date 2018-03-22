70d9c5b067815da7431e93a038ec0dbfdbdb6a13
Stephane Deschamps <stephane.deschamps@gmail.com>
2018-03-22 10:28:57 +0100

    Basic generation: ODS + XLSX

--- generate_tables_wcag2.0.php
+++ generate_tables_wcag2.0.php
@@ -4,27 +4,8 @@
 
 // -----------------------
 // 0.we load contents
-// all the WCAG information
 
-require_once("inc/load_json_wcag2.0.php");
-
-require_once("inc/build_data_arrays.php");
-
-
-
-// -----------------------
-// 1. what format do we want for the output?
-
-// @@ todo : $_GET for html, ods, xlsx
-// $possible_formats = ["ods","xlsx"];
-// for ( $i=0 ; $i < count($possible_formats) ; $i++) {
-// 	echo "<a href=\"{$_SELF}?format={$possible_formats[$i]}\">Generate{$possible_formats[$i]}</a><br>";
-// }
-// exit;
-
-$format = "ods";
-$folder = "output/";
-$filename = "process-matrix-wcag20";
+// spreadsheet material
 
 require 'vendor/autoload.php';
 
@@ -34,7 +15,43 @@ use PhpOffice\PhpSpreadsheet\Writer\Ods;
 
 // init spreadsheet
 $spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
+$spreadsheet->getDefaultStyle()->getFont()->setName('Arial');
+$spreadsheet->getDefaultStyle()->getFont()->setSize(10);
+$worksheet = $spreadsheet->getActiveSheet();
+
+// all the WCAG information
+
+require_once("inc/load_json_wcag2.0.php");
+
+require_once("inc/build_data_arrays.php");
+
+
+
+// -----------------------
+// 1. initiating objects etc.
+
+$folder = "output/";
+$filename = "process-matrix-wcag20";
+$linefeed = "<br>";
+
+$styles_headings = [
+	'font' => [
+		'bold' => true,
+	]
+];
+
+$styles_first_columns = [
+	'alignment' => [
+		'horizontal' => 'left'
+	]
+];
+
+$styles_all = [
+	'alignment' => [
+		'vertical' => 'top',
+		'horizontal' => 'center'
+	]
+];
 
 // -----------------------
 // 2. generating the data for the table
@@ -42,36 +59,36 @@ $sheet = $spreadsheet->getActiveSheet();
 // 2.1. header
 
 # https://phpspreadsheet.readthedocs.io/en/develop/topics/accessing-cells/#setting-a-range-of-cells-from-an-array
-$spreadsheet->getActiveSheet()->fromArray(
+$worksheet->fromArray(
 	$th,
 	NULL,
 	'A1');
 
+$worksheet->getStyle($worksheet->calculateWorksheetDimension())->applyFromArray($styles_headings);
+
+
 // 2.2. data in cells
-$spreadsheet->getActiveSheet()->fromArray(
+$worksheet->fromArray(
 	$tr,
 	NULL,
 	'A2');
+
+$worksheet->getStyle($worksheet->calculateWorksheetDimension())->applyFromArray($styles_all);
+$worksheet->getStyle('A1:C'.$worksheet->getHighestRow())->applyFromArray($styles_first_columns);
 
 // -----------------------
 // 3. outputting the data to a file
 
 // add autofilter to avoid having to create one
-$spreadsheet->getActiveSheet()->setAutoFilter($spreadsheet->getActiveSheet()->calculateWorksheetDimension());
-// echo $spreadsheet->getActiveSheet()->calculateWorksheetDimension();
+$worksheet->setAutoFilter($worksheet->calculateWorksheetDimension());
 
 
-switch ($format) {
-	case 'xlsx':
+
 		$writer = new Xlsx($spreadsheet);
 		$writer->save($folder . $filename . '.xlsx');
-		break;
-
-	default:
+		echo "XLSX created" . $linefeed;
 		$writer = new Ods($spreadsheet);
 		$writer->save($folder . $filename . '.ods');
-		echo "ODS created";
-		break;
-}
+		echo "ODS created" . $linefeed;
 
 ?>
